08f134f1597611d634532c8dc4c485999d366e69
manulpgc <ruanosan1@hotmail.com>
2018-10-05 01:46:15 +0100

    Valores de transferencia estables menos el genero

--- app/src/main/java/com/example/manu/cine/ResultActivity.java
+++ app/src/main/java/com/example/manu/cine/ResultActivity.java
@@ -2,6 +2,7 @@ package com.example.manu.cine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
@@ -14,11 +15,33 @@ public class ResultActivity extends AppCompatActivity {
         String name = getIntent().getExtras().getString("name");
         TextView name_r = (TextView) findViewById(R.id.textView_r_name_2);
         name_r.setText(name);
+
         String lastname = getIntent().getExtras().getString("lastname");
         TextView lastname_r = (TextView) findViewById(R.id.textView_r_lastname_2);
-        name_r.setText(lastname);
+        lastname_r.setText(lastname);
+
         String address = getIntent().getExtras().getString("address");
         TextView address_r = (TextView) findViewById(R.id.textView_r_address_2);
-        name_r.setText(address);
+        address_r.setText(address);
+
+        String country = getIntent().getExtras().getString("country");
+        TextView country_r = (TextView) findViewById(R.id.textView_r_country_2);
+        country_r.setText(country);
+
+        String city = getIntent().getExtras().getString("city");
+        TextView city_r = (TextView) findViewById(R.id.textView_r_city_2);
+        city_r.setText(city);
+
+        String zip = getIntent().getExtras().getString("zip");
+        TextView zip_r = (TextView) findViewById(R.id.textView_r_zip_2);
+        zip_r.setText(zip);
+
+        String phone = getIntent().getExtras().getString("phone");
+        TextView phone_r = (TextView) findViewById(R.id.textView_r_phone_2);
+        phone_r.setText(phone);
+
+        String mail = getIntent().getExtras().getString("mail");
+        TextView mail_r = (TextView) findViewById(R.id.textView_r_mail_2);
+        mail_r.setText(mail);
     }
 }
--- app/src/main/java/com/example/manu/cine/datesActivity.java
+++ app/src/main/java/com/example/manu/cine/datesActivity.java
@@ -7,11 +7,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
 
 public class datesActivity extends AppCompatActivity {
     EditText editTextName;
+    EditText editTextLastname;
+    EditText editTextAddress;
+    EditText editTextCountry;
+    EditText editTextCity;
+    EditText editTextGenre;
+    EditText editTextPhone;
+    EditText editTextZIP;
+    EditText editTextEmail;
     Activity contexto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +32,13 @@ public class datesActivity extends AppCompatActivity {
         contexto = this;
 
         editTextName = findViewById(R.id.editText_name);
-
+        editTextLastname = findViewById(R.id.editText_lastname);
+        editTextAddress = findViewById(R.id.editText_address);
+        editTextCountry = findViewById(R.id.editText_country);
+        editTextCity = findViewById(R.id.editText_city);
+        editTextPhone = findViewById(R.id.editText_phone);
+        editTextZIP = findViewById(R.id.editText_zip);
+        editTextEmail = findViewById(R.id.editText_mail);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,14 +55,23 @@ public class datesActivity extends AppCompatActivity {
 
     private void irAResultado(){
         String name = editTextName.getText().toString();
-        String lastname= editTextName.getText().toString();
-        String address= editTextName.getText().toString();
-
+        String lastname = editTextLastname.getText().toString();
+        String address = editTextAddress.getText().toString();
+        String country = editTextCountry.getText().toString();
+        String city = editTextCity.getText().toString();
+        String phone = editTextPhone.getText().toString();
+        String zip = editTextZIP.getText().toString();
+        String mail = editTextEmail.getText().toString();
         Intent intent = new Intent(contexto, ResultActivity.class);
 
         intent.putExtra("name", name);
         intent.putExtra("lastname", lastname);
         intent.putExtra("address", address);
+        intent.putExtra("country",country);
+        intent.putExtra("city", city);
+        intent.putExtra("phone", phone);
+        intent.putExtra("zip", zip) ;
+        intent.putExtra("mail",mail);
         startActivity(intent);
     }
 }
